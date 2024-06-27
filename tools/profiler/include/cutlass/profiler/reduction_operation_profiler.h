0ea7c35e63e03a947ea8579363a07715b559960d
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-06-27 07:15:06 +0200

    Simplify main.cpp: Step 99.

--- tools/profiler/include/cutlass/profiler/reduction_operation_profiler.h
+++ tools/profiler/include/cutlass/profiler/reduction_operation_profiler.h
@@ -28,10 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  **************************************************************************************************/
-/* \file
-   \brief Defines profiling functionality for reduction operation
-
-*/
 
 #pragma once
 
@@ -41,12 +37,10 @@
 #include <algorithm>
 #include <unordered_map>
 
-// CUTLASS Library includes
 #include "cutlass/library/library.h"
 #include "cutlass/library/util.h"
 #include "cutlass/library/manifest.h"
 
-// Profiler includes
 #include "options.h"
 #include "device_context.h"
 #include "operation_profiler.h"
@@ -56,76 +50,38 @@
 #endif //#if CUTLASS_ENABLE_CUDNN
 #include "debug.h"
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace cutlass {
 namespace profiler {
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// Abstract base class for each math function
 class ReductionOperationProfiler : public OperationProfiler {
 public:
 
-
-  /// Workspace used 
   struct ReductionWorkspace {
 
-    /// Conv device allocations
     DeviceAllocation *Workspace;
     DeviceAllocation *Source;
     DeviceAllocation *Destination;
     DeviceAllocation *Reference;
-    
-    /// Library configuration and arguments
     library::ReductionConfiguration configuration;
     library::ReductionArguments arguments;
 
-    /// Buffer used for the cutlass operations' host workspace
     std::vector<uint8_t> host_workspace;
 
-    /// Buffer used for the cutlass operations' device workspace
     DeviceAllocation device_workspace;
-
-    //
-    // Methods
-    //
 
     ReductionWorkspace(): 
       Workspace(nullptr), Source(nullptr), Destination(nullptr), Reference(nullptr) { }
   };
 
-protected:
-
-  //
-  // Data members
-  //
-
-  /// Reduction problem obtained from problem space
   MatrixCoord problem_;
-
-  /// Device memory allocations 
   ReductionWorkspace conv_workspace_;
 
-
-public:
-  //
-  // Methods
-  //
-
-  /// Ctor
   ReductionOperationProfiler(Options const &options);
 
-  /// Destructor
   virtual ~ReductionOperationProfiler();
 
-  /// Prints usage statement for the math function
-//  virtual void print_usage(std::ostream &out) const;
-
-  /// Prints examples
-//  virtual void print_examples(std::ostream &out) const;
-
-  /// Extracts the problem dimensions
+  /*
   virtual Status initialize_configuration(
     Options const &options, 
     PerformanceReport &report, 
@@ -134,7 +90,6 @@ public:
     ProblemSpace const &problem_space,
     ProblemSpace::Problem const &problem);
 
-  /// Initializes workspace
   virtual Status initialize_workspace(
     Options const &options, 
     PerformanceReport &report, 
@@ -143,7 +98,6 @@ public:
     ProblemSpace const &problem_space,
     ProblemSpace::Problem const &problem);
 
-  /// Measures performance results
   virtual bool profile(
     Options const &options, 
     PerformanceReport &report, 
@@ -151,7 +105,7 @@ public:
     library::Operation const *operation,
     ProblemSpace const &problem_space,
     ProblemSpace::Problem const &problem);
-  
+*/  
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
