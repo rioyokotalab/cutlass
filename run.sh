a256451cdc9dd6d3b71f67f4ab22c26426e72680
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-08-22 15:39:25 +0900

    Simplify CMake: Step 12.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCUTLASS_NVCC_ARCHS='90a' -DCUTLASS_LIBRARY_KERNELS=cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8
+cmake .. -DCUTLASS_LIBRARY_KERNELS=cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8
 make cutlass_profiler -j16
 ./tools/profiler/cutlass_profiler
 
