e0a790ae168d6bb996f380ce800d1a2e7661e7ba
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-08-23 18:55:52 +0900

    Removing unused files: Step 7.

--- src/cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8.cu
+++ src/cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8.cu
@@ -60,7 +60,8 @@ namespace library {
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-void initialize_all_gemm_operations(Manifest &manifest) {
+void initialize_all(Manifest &manifest) {
+  manifest.reserve(1);
   manifest.append(new GemmUniversalOperation<
       cutlass::gemm::device::GemmUniversalAdapter<cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8>
     >("cutlass_tensorop_s16816gemm_f16_128x128_32x5_nt_align8"));
