ba600a1896fae806dd7b617c725901dc3db8b7dc
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2024-12-04 18:13:41 +0900

    Define ptr_A,B,C,D in gemm_universal_adapter.h.

--- include/cutlass/gemm/device/gemm_universal_adapter.h
+++ include/cutlass/gemm/device/gemm_universal_adapter.h
@@ -71,73 +71,6 @@ namespace cutlass::gemm::device {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////// CUTLASS 2.x API /////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-/*
-  struct Params {
-    cutlass::gemm::GemmCoord problem_size;
-    cutlass::gemm::GemmCoord grid_tiled_shape;
-    int swizzle_log_tile;
-    typename Mma::IteratorA::Params params_A;
-    typename Mma::IteratorA::TensorRef ref_A;
-    typename Mma::IteratorB::Params params_B;
-    typename Mma::IteratorB::TensorRef ref_B;
-    typename Epilogue::OutputTileIterator::Params params_C;
-    typename Epilogue::OutputTileIterator::TensorRef ref_C;
-    typename Epilogue::OutputTileIterator::Params params_D;
-    typename Epilogue::OutputTileIterator::TensorRef ref_D;
-    typename OutputOp::Params output_op;
-    int *semaphore;
-    int gemm_k_size;
-    // For gather+scatter operations
-    int const *gather_A_indices;
-    int const *gather_B_indices;
-    int const *scatter_D_indices;
-
-    //
-    // Methods
-    //
-
-    CUTLASS_HOST_DEVICE
-    Params(): swizzle_log_tile(0), semaphore(0), gemm_k_size(0) { }
-
-    CUTLASS_HOST_DEVICE
-    Params(
-      cutlass::gemm::GemmCoord const & problem_size,
-      cutlass::gemm::GemmCoord const & grid_tiled_shape,
-      typename Mma::IteratorA::TensorRef ref_A,
-      typename Mma::IteratorB::TensorRef ref_B,
-      typename Epilogue::OutputTileIterator::TensorRef ref_C,
-      typename Epilogue::OutputTileIterator::TensorRef ref_D,
-      typename OutputOp::Params output_op = typename OutputOp::Params(),
-      int *workspace = nullptr,
-      int const *gather_A_indices = nullptr,
-      int const *gather_B_indices = nullptr,
-      int const *scatter_D_indices = nullptr
-    ):
-      problem_size(problem_size),
-      grid_tiled_shape(grid_tiled_shape),
-      swizzle_log_tile(ThreadblockSwizzle().get_log_tile(grid_tiled_shape)),
-      params_A(ref_A.layout()),
-      ref_A(ref_A),
-      params_B(ref_B.layout()),
-      ref_B(ref_B),
-      params_C(ref_C.layout()),
-      ref_C(ref_C),
-      params_D(ref_D.layout()),
-      ref_D(ref_D),
-      output_op(output_op),
-      gather_A_indices(gather_A_indices),
-      gather_B_indices(gather_B_indices),
-      scatter_D_indices(scatter_D_indices) {
-
-      int total_gemm_k_iterations = (problem_size.k() + Mma::Shape::kK - 1) / Mma::Shape::kK;
-      int gemm_k_iterations = (total_gemm_k_iterations + grid_tiled_shape.k() - 1) / grid_tiled_shape.k();
-      
-      gemm_k_size = gemm_k_iterations * Mma::Shape::kK;
-
-    semaphore = workspace;
-    }
-  };
-*/
 template <class GemmKernel_>
 class GemmUniversalAdapter
 {
@@ -268,6 +201,20 @@ public:
 
     init_device_props();
     params_ = typename GemmKernel::Params(to_underlying_arguments(args), device_sms_, sm_occupancy_);
+    params_.problem_size = cutlass::gemm::GemmCoord(3456,4096,4096);
+    params_.grid_tiled_shape = cutlass::gemm::GemmCoord(32,27,1);
+    typename GemmKernel::Mma::IteratorA::Params params_A(make_Coord(long(4096)));
+    typename GemmKernel::Mma::IteratorB::Params params_B(make_Coord(long(3456)));
+    typename GemmKernel::Epilogue::OutputTileIterator::Params params_C(make_Coord(long(3456)));
+    typename GemmKernel::Epilogue::OutputTileIterator::Params params_D(make_Coord(long(3456)));
+    params_.params_A = params_A;
+    params_.params_B = params_B;
+    params_.params_C = params_C;
+    params_.params_D = params_D;
+    params_.ptr_A = const_cast<void *>(args.ptr_A);
+    params_.ptr_B = const_cast<void *>(args.ptr_B);
+    params_.ptr_C = const_cast<void *>(args.ptr_C);
+    params_.ptr_D = args.ptr_D;
     return params_.init_workspace(workspace, stream);
   }
 
@@ -282,20 +229,9 @@ public:
     cudaStream_t stream = nullptr,
     CudaHostAdapter *cuda_adapter = nullptr) {
 
-    dim3 block(GemmKernel::kThreadCount, 1, 1);
-    dim3 grid = params_.get_grid_dims();
-    size_t kSharedStorageSize = sizeof(typename GemmKernel::SharedStorage);
-    params_.problem_size = cutlass::gemm::GemmCoord(3456,4096,4096);
-    params_.grid_tiled_shape = cutlass::gemm::GemmCoord(32,27,1);
-    typename GemmKernel::Mma::IteratorA::Params params_A(make_Coord(long(4096)));
-    typename GemmKernel::Mma::IteratorB::Params params_B(make_Coord(long(3456)));
-    typename GemmKernel::Epilogue::OutputTileIterator::Params params_C(make_Coord(long(3456)));
-    typename GemmKernel::Epilogue::OutputTileIterator::Params params_D(make_Coord(long(3456)));
-    params_.params_A = params_A;
-    params_.params_B = params_B;
-    params_.params_C = params_C;
-    params_.params_D = params_D;
-    Kernel2<GemmKernel><<<grid, block, kSharedStorageSize, stream>>>(params_);
+    dim3 block(128, 1, 1);
+    dim3 grid(256, 4, 1);
+    Kernel2<GemmKernel><<<grid, block, 81920, stream>>>(params_);
 
     cudaError_t result = cudaGetLastError();
     if (result != cudaSuccess) {
